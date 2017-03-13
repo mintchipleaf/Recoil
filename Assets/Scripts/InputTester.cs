25a5f470ec415d770c1981af3b741b49934d0680
mint <mintchipleaf@gmail.com>
2017-03-12 17:53:04 -0700

    Add InControl

---
+++ Assets/Scripts/InputTester.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using InControl;
+
+public class InputTester : MonoBehaviour {
+
+	public bool rightStick = false;
+	private float leftX;
+	private float leftY;
+	private float rightX;
+	private float rightY;
+    
+	void Update () {
+		var inputDevice = InputManager.ActiveDevice;
+		
+		Vector2 pos;
+		if(rightStick)
+		{
+			rightX = inputDevice.LeftStickY.Value;
+			rightY = inputDevice.RightStickY.Value * -1;
+			pos = new Vector2(rightX, rightY);
+		}
+		else
+		{
+			leftX = inputDevice.LeftStickX.Value;
+			leftY = inputDevice.RightStickX.Value * -1;
+			pos = new Vector2(leftX, leftY);
+		}
+		
+		if(rightStick){
+			Debug.Log(pos);
+		}
+		transform.position = pos;
+	}
+}
